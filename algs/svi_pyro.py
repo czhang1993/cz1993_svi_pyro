daa47955d435d8e6c103e1f1a442bdb2c1b55919
Calvin Zhang 1993 <121672580+czhang1993@users.noreply.github.com>
2023-05-10 11:16:49 +1000

    Update svi_pyro.py

--- algs/svi_pyro.py
+++ algs/svi_pyro.py
@@ -3,15 +3,18 @@ from pyro import sample
 from pyro.distributions import Normal
 from pyro.infer import SVI, Trace_ELBO
 
+
 def model(data):
     mu = Tensor(0.0)
     sigma = Tensor(1.0)
-    z = sample("z", Normal(mu, sigma))
+    z = sample(
+        name="z",
+        fn=Normal(mu, sigma)
+    )
     for i in range(n):
         sample(
-            ?,
-            Normal(z),
-            ?
+            name=?,
+            fn=Normal(z)
         )
 
 
